9a592abea9b2bc28a4ab34660757ca7b5dd10c0a
Paul Obayuwana <paul.scholes50@yahoo.com>
2024-02-02 20:24:41 -0500

    add middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
@@ -8,9 +10,11 @@ export default function Home() {
           you have to do is be honest.
         </p>
         <div>
-          <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
-            get started
-          </button>
+          <Link href="/Journal">
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+              get started
+            </button>
+          </Link>
         </div>
       </div>
     </div>
