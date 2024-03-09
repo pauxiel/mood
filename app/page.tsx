052dc2a174061ba1734ac1ee6494e1bf9ff8a738
Paul Obayuwana <66179327+pauxiel@users.noreply.github.com>
2024-03-09 15:56:01 -0500

    Prisma databse setup (#4)
    * add button
    
    * add clerk and signup page
    
    * add sigin page
    
    * add middleware
    
    * setting up prisma with railway database
    
    * Update .env

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
           you have to do is be honest.
         </p>
         <div>
-
           <Link href="/Journal">
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
               get started
