218295ef5064391f096d7033fda289d980b424fd
Paul Obayuwana <paul.scholes50@yahoo.com>
2024-03-19 13:47:50 -0400

    selecting journal entry

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,23 @@
-const JournalPage = () => {
-  return <div>Journal</div>
+import { prisma } from '@/util/db'
+import { getUserByClerkID } from '@/util/auth'
+const getEntries = async () => {
+  const user = await getUserByClerkID()
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  return entries
+}
+
+const JournalPage = async () => {
+  const entries = await getEntries()
+  console.log('entries ', entries)
+  return <div>journal</div>
 }
 
 export default JournalPage
